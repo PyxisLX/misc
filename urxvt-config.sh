8a5af9f4b1ada1f3890f7b0d8b1b019ee9212004
pyxislx <pyxislx@gmail.com>
2014-02-28 19:55:55 +0800

    Initial commit.

---
+++ urxvt-config.sh
@@ -0,0 +1,1 @@
+./configure --prefix=/usr/local --enable-256-color --enable-unicode3 --enable-combining --enable-xft --enable-font-styles --enable-pixbuf --enable-transparency --enable-fading --enable-xim
---
+++ vimrc
@@ -0,0 +1,10 @@
+syntax on
+set ts=4
+colorscheme jellybeans
+set background=dark
+set t_Co=256
+
+set rtp+=$GOROOT/misc/vim
+filetype plugin indent on
+filetype plugin on
+syntax on
